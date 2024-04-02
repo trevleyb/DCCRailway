1a50af5a13519a1ed650b1299110db93ab2e0931
Trevor Leybourne <trevor@theleybournes.com>
2024-04-03 07:36:40 +1300

    Refactor LayoutLocoCmdUpdater to check command type
    The update in the code changes the logic for accessing locomotives in the configuration. Now, it first checks if the incoming command is of type 'ILocoCmd' before attempting to access any locomotives. This avoids errors and unnecessary logging in case of non-matching types. It also adds error logging for unsupported command types outside the switch block.

--- DCCRailway/LayoutCmdUpdater/LayoutCmdUpdaters/LayoutLocoCmdUpdater.cs
+++ DCCRailway/LayoutCmdUpdater/LayoutCmdUpdaters/LayoutLocoCmdUpdater.cs
@@ -12,40 +12,44 @@ public class LayoutLocoCmdUpdater(DCCRailwayConfig config) : LayoutGenericCmdUpd
     public new bool Process(ICommand command) {
         // Get the Accessory from the configuration so that we can update its state
         // -----------------------------------------------------------------------------
-        var loco = Config.Locomotives[((ILocoCmd)command).Address];
-        if (loco is null) {
-            Logger.Log.Error($"Command {command.AttributeInfo().Name} - no matching Accessory {((IAccyCmd)command).Address.Address}.");
-            return false;
-        }
+        if (command is ILocoCmd locoCmd) {
+            var loco = Config.Locomotives[locoCmd.Address];
+            if (loco is null) {
+                Logger.Log.Error(
+                    $"Command {command.AttributeInfo().Name} - no matching Accessory {((IAccyCmd)command).Address.Address}.");
+                return false;
+            }
 
-        switch (command) {
-        case ICmdLocoStop cmd:
-            loco.LastDirection = loco.Direction;
-            loco.LastSpeed     = loco.Speed;
-            loco.Direction     = DCCDirection.Stop;
-            loco.Speed         = new DCCSpeed(0);
-            break;
-        case ICmdLocoSetFunctions cmd:
-            break;
-        case ICmdLocoSetFunction cmd:
-            break;
-        case ICmdLocoSetMomentum cmd:
-            loco.Momentum = cmd.Momentum;
-            break;
-        case ICmdLocoSetSpeed cmd:
-            loco.Speed     = cmd.Speed;
-            loco.LastSpeed = cmd.Speed;
-            break;
-        case ICmdLocoSetSpeedSteps cmd:
-            break;
-        case ICmdLocoOpsProg cmd:
-            break;
-        default:
+            switch (command) {
+            case ICmdLocoStop cmd:
+                loco.LastDirection = loco.Direction;
+                loco.LastSpeed     = loco.Speed;
+                loco.Direction     = DCCDirection.Stop;
+                loco.Speed         = new DCCSpeed(0);
+                break;
+            case ICmdLocoSetFunctions cmd:
+                break;
+            case ICmdLocoSetFunction cmd:
+                break;
+            case ICmdLocoSetMomentum cmd:
+                loco.Momentum = cmd.Momentum;
+                break;
+            case ICmdLocoSetSpeed cmd:
+                loco.Speed     = cmd.Speed;
+                loco.LastSpeed = cmd.Speed;
+                break;
+            case ICmdLocoSetSpeedSteps cmd:
+                break;
+            case ICmdLocoOpsProg cmd:
+                break;
+            default:
+                Logger.Log.Error($"Command {command.AttributeInfo().Name} not supported.");
+                throw new Exception("Unexpected type of command executed.");
+            }
+        }
+        else {
             Logger.Log.Error($"Command {command.AttributeInfo().Name} not supported.");
-
-            throw new Exception("Unexpected type of command executed.");
         }
-
         return true;
     }
 }
