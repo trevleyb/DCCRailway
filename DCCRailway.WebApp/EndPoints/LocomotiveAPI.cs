949a8a4d8411456674d0edadecb79006eafbf6bb
Trevor Leybourne <trevor@theleybournes.com>
2024-05-13 10:53:05 +1200

    Moved the WebApp to its own project (again) to simplify the main app and want to just focus on getting the background processes working before looking at any front end

---
+++ DCCRailway.WebApp/EndPoints/LocomotiveAPI.cs
@@ -0,0 +1,40 @@
+using DCCRailway.Layout.Collection;
+using DCCRailway.Layout.Entities;
+
+namespace DCCRailway.Railway.EndPoints;
+
+public static class LocomotiveAPI {
+    public static void Configure(WebApplication app, ILayoutRepository<Locomotive> entities) {
+
+        app.MapGet("/layout/locomotives/{id}", async (string id) => {
+            var locomotive = await entities.GetByIDAsync(id);
+            return locomotive == null ? Results.NotFound() : Results.Ok(locomotive);
+        });
+
+        app.MapGet("/layout/locomotives", async () => await Task.FromResult(entities));
+        app.MapPost("/layout/locomotives", async (Locomotive locomotive) => Results.Ok(await entities.AddAsync(locomotive)));
+        app.MapPut("/layout/locomotives/{id}", async(string id, Locomotive locomotive) => Results.Ok(await entities.UpdateAsync(locomotive)));
+        app.MapDelete("/layout/locomotives/{id}", async (string id) => Results.Ok(await entities.DeleteAsync(id)));
+    }
+}
+
+
+/* Use this for search of Locomotives
+
+app.MapGet("/manufacturers", async ([FromQuery] Dictionary<string, string> queryParameters) => {
+       var manufacturers = config.SystemEntities.Manufacturers.AsQueryable();
+
+       foreach (var param in queryParameters)
+       {
+           var propertyInfo = typeof(Manufacturer).GetProperty(param.Key, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+           if (propertyInfo != null)
+           {
+               manufacturers = manufacturers.Where(m => propertyInfo.GetValue(m, null)?.ToString().Contains(param.Value, StringComparison.InvariantCultureIgnoreCase) == true);
+           }
+       }
+
+       var results = await manufacturers.ToListAsync();
+       return results.Count == 0 ? Results.NotFound("No manufacturers found matching the provided criteria.") : Results.Ok(results);
+   });
+
+*/
---
+++ DCCRailway.WebApp/EndPoints/SignalAPI.cs
@@ -0,0 +1,19 @@
+using DCCRailway.Layout.Collection;
+using DCCRailway.Layout.Entities;
+
+namespace DCCRailway.Railway.EndPoints;
+
+public static class SignalAPI {
+    public static void Configure(WebApplication app, ILayoutRepository<Signal> entities) {
+
+        app.MapGet("/layout/signals/{id}", async (string id) => {
+            var signal = await entities.GetByIDAsync(id);
+            return signal == null ? Results.NotFound() : Results.Ok(signal);
+        });
+
+        app.MapGet("/layout/signals", async () => await Task.FromResult(Results.Ok(entities.GetAllAsync())));
+        app.MapPost("/layout/signals", async (Signal signal) => Results.Ok(await entities.AddAsync(signal)));
+        app.MapPut("/layout/signals/{id}", async (string id, Signal signal) => Results.Ok(await entities.UpdateAsync(signal)));
+        app.MapDelete("/layout/signals/{id}", async (string id) => Results.Ok(await entities.DeleteAsync(id)));
+    }
+}
