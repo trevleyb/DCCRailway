374d931b8e51227d4f9e2b7cab430b1795e84dba
trevor <trevor@theleybournes.com>
2024-09-04 08:54:49 +1200

    Add DccRailway.Server and DccRailway.Server.Client projects with initial setup and components.

---
+++ DccRailway.Server/DccRailway.Server/Program.cs
@@ -0,0 +1,32 @@
+using DccRailway.Server.Components;
+using MudBlazor.Services;
+using _Imports = DccRailway.Server.Client._Imports;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents().AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseWebAssemblyDebugging();
+} else {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode().AddInteractiveWebAssemblyRenderMode().AddAdditionalAssemblies(typeof(_Imports).Assembly);
+
+app.Run();
